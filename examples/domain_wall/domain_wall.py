3c248a8b79936a37f05f5fed55827067515392fb
mb4e10 <mb4e10@soton.ac.uk>
2015-09-09 14:17:05 +0100

    Add domain wall example (finite difference code comparison with analytic solution.)

---
+++ examples/domain_wall/domain_wall.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from fidimag.micro import Sim
+from fidimag.micro import FDMesh
+from fidimag.micro import UniformExchange
+from fidimag.micro import UniaxialAnisotropy
+
+# Material parameters
+Ms = 8.6e5  # saturation magnetisation (A/m)
+A = 1.3e-11  # exchange energy constant (J/m)
+K = 5e3  # uniaxial anisotropy (J/m**3)
+alpha = 5  # Gilbert damping
+gamma = 2.211e5  # gyromagnetic ratio
+
+############
+# Simulation
+############
+# Finite difference 1D mesh.
+mesh = FDMesh(nx=500, ny=1, nz=1, dx=2, dy=2, dz=2, unit_length=1e-9)
+
+
+# Initial magnetisation function.
+def init_m(pos):
+    x = pos[0]
+    if x < 450:
+        return (1, 0, 0)
+    elif 450 <= x < 550:
+        return (0, 1, 1)
+    else:
+        return (-1, 0, 0)
+
+sim = Sim(mesh)
+sim.alpha = alpha
+sim.gamma = gamma
+sim.Ms = Ms
+sim.add(UniformExchange(A))
+sim.add(UniaxialAnisotropy(K))
+sim.set_m(init_m)
+sim.relax(dt=1e-13, stopping_dmdt=0.001, max_steps=5000)
+
+# Magnetisation x component.
+mx_simulation = sim.spin.reshape((len(sim.spin)/3, 3))[:, 0]
+
+###################
+# Analytic solution
+###################
+x_array = np.linspace(0, 1000e-9, 500)
+mx_analytic = -np.tanh((x_array-500e-9)/np.sqrt(A/K))
+
+###################
+# Plot solutions
+###################
+plt.figure(figsize=(8, 5))
+plt.plot(x_array/1e-9, mx_simulation, 'o', label='simulation')
+plt.plot(x_array/1e-9, mx_analytic, linewidth=2, label='analytic')
+plt.xlabel('x (nm)')
+plt.ylabel('mx')
+plt.grid()
+plt.legend()
+plt.savefig('domain_wall.pdf', format='pdf', bbox_inches='tight')
