2d62feed23a00d7997cd444e3ab14f664589961e
Hans Fangohr <fangohr@soton.ac.uk>
2015-11-13 12:13:34 +0000

    Adding some tests for easy of checking package

---
+++ test_package.py
@@ -0,0 +1,15 @@
+def test_can_import():
+    import micromagnetictestcases
+    micromagnetictestcases
+
+
+def test_can_access_macrospin_solution():
+    import micromagnetictestcases
+    s = micromagnetictestcases.macrospin.solution
+    s
+
+
+def test_can_access_domainwall_solution():
+    import micromagnetictestcases
+    s = micromagnetictestcases.domainwall.solution
+    s
