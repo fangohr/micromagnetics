87ea834234f44c2728a4f9ec77f900313ba083d3
Hans Fangohr <fangohr@soton.ac.uk>
2015-11-13 13:07:21 +0000

    pep8 improvements

--- micromagnetictestcases/domainwall/analytic_solution.py
+++ micromagnetictestcases/domainwall/analytic_solution.py
@@ -7,6 +7,6 @@ def domainwall_analytic_solution(A, K, d, n_points):
     in absence of an external magnetic field.
     """
     x_array = np.linspace(0, d, n_points)
-    mx_analytic = -np.tanh((x_array-d/2)/np.sqrt(A/K))
+    mx_analytic = -np.tanh((x_array - d / 2.) / np.sqrt(A / K))
 
     return mx_analytic
